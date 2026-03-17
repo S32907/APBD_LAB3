bb59888c3354f94c234d0e82be12a0fef1c022e8
Oleksandr Iievliev <sXXXX@pjwstk.edu.pl>
2026-03-17 13:41:30 +0100

    "Crated new User clsses using Abstract User class"

---
+++ E_rental_service/E_rental_service/Entities/Teacher.cs
@@ -0,0 +1,26 @@
+﻿namespace E_rental_service.Entities;
+using E_rental_service.Enums;
+public class Teacher : User
+{
+    //properties
+    public double PopularityRating 
+    {
+        get {return PopularityRating;}
+        set 
+        {
+            if (value < 0 || value > 5)
+            {
+                throw new ArgumentOutOfRangeException(nameof(value), "PopularityRating must be in range from 0 to 5.");
+            }
+            PopularityRating = value;
+        }
+    }
+    
+    //constructor
+    public Teacher(string identifier, string firstName,
+        string lastName, UserTypes type, double popularityRating) : base(identifier, firstName, lastName, type)
+    {
+        PopularityRating = popularityRating;
+    }
+    
+}
